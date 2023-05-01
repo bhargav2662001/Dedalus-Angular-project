dfe48fecea203691a4ff21dbea81687b01a3abc3
bhargav <bhargav.cse2001@gmail.com>
2023-05-01 20:25:35 +0530

    first commit

--- Online food ordering system/src/app/services/food/food.service.ts
+++ Online food ordering system/src/app/services/food/food.service.ts
@@ -11,12 +11,9 @@ export class FoodService {
       '/assets/images/foods/food-1.jpg',
       '/assets/images/foods/food-3.jpg',
       '/assets/images/foods/food-4.jpg',
-      '/assets/images/foods/food-6.jpg',
       '/assets/images/foods/food-7.jpg',
       '/assets/images/foods/food-8.jpg',
       '/assets/images/foods/food-9.jpg',
-      
-
     ]
   }
 }
